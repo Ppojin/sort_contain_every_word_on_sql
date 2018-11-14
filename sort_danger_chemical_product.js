f07498085a3fd248a5a819b9470134f38299aa3e
Ppojin <25076967+Ppojin@users.noreply.github.com>
2018-11-14 11:35:46 +0900

    road

--- sort_danger_chemical_product.js
+++ sort_danger_chemical_product.js
@@ -1,10 +1,10 @@
 import "lib/prototype.js";
 
 function sorting(dangerList, sqlCon, tableName, chemicalNameColumn, idColumn){
-    if(dangerList.isArray){
+    if(!(dangerList.isArray)){
         return error(dangerList.Prototype)
     }
     for (dangerChem in dangerList){
-
+        
     }
 }
